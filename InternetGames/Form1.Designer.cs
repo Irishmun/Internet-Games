28e00007797faf90b0424a2110f1a279d97f59e5
Irishmun <steefiscool70@gmail.com>
2022-02-23 15:53:22 +0100

    Initial project create

---
+++ InternetGames/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace InternetGames
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PB_CheckersField = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_CheckersField)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_CheckersField
+            // 
+            this.PB_CheckersField.Image = global::InternetGames.Properties.Resources.Checkers_Field;
+            this.PB_CheckersField.InitialImage = global::InternetGames.Properties.Resources.Checkers_Field;
+            this.PB_CheckersField.Location = new System.Drawing.Point(0, 0);
+            this.PB_CheckersField.Name = "PB_CheckersField";
+            this.PB_CheckersField.Size = new System.Drawing.Size(540, 360);
+            this.PB_CheckersField.TabIndex = 0;
+            this.PB_CheckersField.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::InternetGames.Properties.Resources.Checkers_Pieces;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 366);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 37);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 531);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.PB_CheckersField);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(556, 574);
+            this.MinimumSize = new System.Drawing.Size(556, 43);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Magenta;
+            ((System.ComponentModel.ISupportInitialize)(this.PB_CheckersField)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_CheckersField;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
